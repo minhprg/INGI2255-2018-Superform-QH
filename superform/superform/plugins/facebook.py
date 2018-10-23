0bcfff478afccd4cba1dc3bc281f867a67826058
tponchau <thomas.ponchau@student.uclouvain.be>
2018-10-23 11:57:21 +0200

    Function getting pages

--- superform/superform/plugins/facebook.py
+++ superform/superform/plugins/facebook.py
@@ -6,7 +6,7 @@ from flask import Blueprint, url_for, app
 
 FIELDS_UNAVAILABLE = ['Title']
 
-CONFIG_FIELDS = ["access_token"]
+CONFIG_FIELDS = ["access_token","page"]
 
 
 def get_url_for_token(id_channel):
@@ -24,3 +24,10 @@ def run(publishing,channel_config):
     graph = facebook.GraphAPI(access_token=acc_tok)
     graph.put_object(parent_object='me', connection_name='feed', message=publishing.description,
                      link=publishing.link_url)
+
+def get_page(channel_config):
+    json_data = json.loads(channel_config)
+    acc_tok = json_data['access_token']
+    graph = facebook.GraphAPI(access_token=acc_tok)
+    pages = graph.get_connections(id='me', connection_name='pages')
+    return pages

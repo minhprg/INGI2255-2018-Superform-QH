9d4153a63a71f77a03c99d2f6c6a133ec03145ff
destmaxi <maxime.destreel@student.uclouvain.be>
2018-10-25 22:35:51 +0200

    Fix: Reboot of server destroy the rss.xml file
    When the server reboots it whas deleeting all the rss links in the xml,
    add also the fact that if no xml file exist it creates one

--- superform/superform/plugins/rss.py
+++ superform/superform/plugins/rss.py
@@ -1,16 +1,16 @@
-
 import PyRSS2Gen
 import datetime
 import json
+from pathlib import Path
+from lxml import etree
+
 
 FIELDS_UNAVAILABLE = ["date_from", "date_until", "image"]
 CONFIG_FIELDS = ["feedUrl"]
 rss = None
 
 
-def createInitialFeed(channel_config):
-    json_data = json.loads(channel_config)
-    destination = json_data['feedUrl']
+def createInitialFeed():
 
     global rss
     rss = PyRSS2Gen.RSS2(
@@ -19,18 +19,51 @@ def createInitialFeed(channel_config):
         description="The RSS feed of Superform",
         items=[]
     )
-    rss.write_xml(open(destination, "w"))
 
     return rss
 
 
 def run(publishing, channel_config):
+    global rss
+
     json_data = json.loads(channel_config)
     destination = json_data["feedUrl"]
 
-    global rss
-    if rss is None:
-        rss = createInitialFeed(channel_config)
+    file = Path(destination)
+
+    if not file.exists():
+        rss = createInitialFeed()
+    else:
+        tree = etree.parse(destination)
+
+        if rss is None:
+            rss = createInitialFeed()
+
+            title = list()
+            link = list()
+            description = list()
+            pubDate = list()
+
+            for attrib in tree.xpath("/rss/channel/item/title"):
+                title.append(attrib.text)
+
+            for attrib in tree.xpath("/rss/channel/item/link"):
+                link.append(attrib.text)
+
+            for attrib in tree.xpath("/rss/channel/item/description"):
+                description.append(attrib.text)
+
+            for attrib in tree.xpath("/rss/channel/item/pubDate"):
+                pubDate.append(attrib.text)
+
+            for i in range(0, len(title) - 1):
+                item = PyRSS2Gen.RSSItem(
+                    title=title[i],
+                    link=link[i],
+                    description=description[i],
+                    pubDate=pubDate[i]
+                )
+                rss.items.insert(0, item)
 
     pubDate = datetime.datetime.now()
 
@@ -41,7 +74,11 @@ def run(publishing, channel_config):
                 pubDate=pubDate)
 
     rss.items.insert(0, item)
+    '''   
     for post in rss.items:
         if post.pubDate + datetime.timedelta(365) < pubDate:
-            rss.items.remove(post)
-    rss.write_xml(open(destination, "w"))
+            rss.items.remove(post) 
+    '''
+    file = open(destination, "w+")
+    rss.write_xml(file)
+    file.close()

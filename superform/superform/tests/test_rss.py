b0a2236320712a4b31b8a003422d8e3ff00ad1e4
destmaxi <maxime.destreel@student.uclouvain.be>
2018-10-25 22:52:03 +0200

    Add a test to verify if a new RSS was created if non exists

--- superform/superform/tests/test_rss.py
+++ superform/superform/tests/test_rss.py
@@ -1,13 +1,28 @@
 import unittest
 import xml.etree.ElementTree as ET
+import os
+from pathlib import Path
+
 
 from superform import *
 from superform.plugins import rss
 
 class TestRSS(unittest.TestCase):
 
-    def test_publish_post(self):
+    def test_create_feed_if_none_exist(self):
         pub = Publishing(title="coucou", description="ta mere", link_url="www.facebook.com")
+        os.system("rm /Users/maxime/serv/rss.xml")
+
+        conf = "{\"feedUrl\": \"/Users/maxime/serv/rss.xml\"}"
+
+        rss.run(pub, conf)
+
+        file = Path("/Users/maxime/serv/rss.xml")
+
+        self.assertTrue(file.exists())
+
+    def test_publish_post(self):
+        pub = Publishing(title="Voici un super beau titre", description="Avec une super description", link_url="www.facebook.com", date_from="2018-10-25")
         conf = "{\"feedUrl\": \"/Users/maxime/serv/rss.xml\"}"
 
         rss.run(pub, conf)

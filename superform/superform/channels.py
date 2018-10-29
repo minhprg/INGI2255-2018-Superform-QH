d3e56fe92713d623b7414892bc0a95eb237f6fca
chenoya <chenoya@users.noreply.github.com>
2018-10-29 14:39:35 +0100

    Add comments + restructure code

--- superform/superform/channels.py
+++ superform/superform/channels.py
@@ -1,5 +1,3 @@
-import json
-
 from flask import Blueprint, current_app, url_for, request, make_response, redirect, session, render_template
 
 from superform.utils import login_required, get_instance_from_module_path, get_modules_names, get_module_full_name
@@ -55,7 +53,7 @@ def configure_channel(id):
         if m == 'superform.plugins.facebook':
             return render_template("channel_configure_facebook.html", channel=c, config_fields=config_fields,
                                    url_token=clas.get_url_for_token(id),
-                                   pages=clas.get_page(c.config_dict.get("access_token")))
+                                   pages=clas.get_list_user_pages(c.config_dict.get("access_token")))
         else:
             return render_template("channel_configure.html", channel=c, config_fields=config_fields)
     str_conf = "{"
@@ -74,23 +72,30 @@ def configure_channel(id):
     db.session.commit()
     return redirect(url_for('channels.channel_list'))
 
+
 @channels_page.route("/callback_fb", methods=['GET', 'POST'])
 @login_required(admin_required=True)
 def callback_fb():
-
+    """Page where Facebook returns the code to get the access token.
+        Generate the access token from the code and save it to the DB."""
     id_channel = request.args.get('state')
     code = request.args.get('code')
+    if id_channel is None:
+        return redirect(url_for("channels.channel_list"))
 
-    app_id = "1672680826169132"
-    canvas_url = "https://127.0.0.1:5000/callback_fb"
+    app_id = current_app.config["FACEBOOK_APP_ID"]
+    app_secret = current_app.config["FACEBOOK_APP_SECRET"]
+    canvas_url = url_for('channels.callback_fb', _external=True)
     graph = facebook.GraphAPI()
-    res = graph.get_access_token_from_code(code, canvas_url, app_id, "152bd148cea3d9c7cacaa6cc234546ff")
+    try:
+        res = graph.get_access_token_from_code(code, canvas_url, app_id, app_secret)
+        access_token = res['access_token']
+    except facebook.GraphAPIError:
+        access_token = 'Unable to generate access_token'
 
-    access_token = res['access_token']
     channel = Channel.query.get(id_channel)
     # reset config and add new access_token
     channel.config = "{\"access_token\": \"" + str(access_token) + "\"}"
 
     db.session.commit()
     return redirect(url_for("channels.configure_channel", id=id_channel))
-
--- superform/superform/plugins/facebook.py
+++ superform/superform/plugins/facebook.py
@@ -1,46 +1,70 @@
 import facebook
 import json
 
-from superform.utils import login_required
-from flask import Blueprint, url_for, app
+from flask import url_for, current_app
 
 FIELDS_UNAVAILABLE = ['Title']
 
-CONFIG_FIELDS = ["access_token","page"]
+CONFIG_FIELDS = ["access_token", "page"]
+
+
+def run(publishing, channel_config):
+    """Publish the post on the selected page on Facebook"""
+    json_data = json.loads(channel_config)
+    acc_tok = json_data['access_token']
+    page_id = json_data['page']
+    page = get_page_from_id(acc_tok, page_id)
+    if page is None:
+        print("Invalid page ID")
+        # TODO should add log here
+        return
+    try:
+        graph = facebook.GraphAPI(access_token=page['access_token'])
+        # check token validity
+        debug = graph.debug_access_token(
+            page['access_token'],
+            current_app.config["FACEBOOK_APP_ID"],
+            current_app.config["FACEBOOK_APP_SECRET"])
+        if not debug['data']['is_valid']:
+            print("Invalid Access-Token")
+            # TODO should add log here
+            return
+        # publish post
+        graph.put_object(
+            parent_object="me",
+            connection_name="feed",
+            message=publishing.description,
+            link=publishing.link_url)
+    except facebook.GraphAPIError:
+        print("GraphAPIError")
+        # TODO should add log here
+        return
 
 
 def get_url_for_token(id_channel):
-    app_id = "1672680826169132"
-    canvas_url = "https://127.0.0.1:5000/callback_fb"
+    """Return an URL to Facebook to get a valid access token."""
+    app_id = current_app.config["FACEBOOK_APP_ID"]
+    canvas_url = url_for('channels.callback_fb', _external=True)
     perms = ["manage_pages", "publish_pages"]
     graph = facebook.GraphAPI()
     url = graph.get_auth_url(app_id, canvas_url, perms)
     return url + "&state=" + str(id_channel)
 
 
-def run(publishing,channel_config):
-    json_data = json.loads(channel_config)
-    acc_tok = json_data['access_token']
-    page_id = json_data['page']
-    page = get_page_from_id(acc_tok, page_id)
-    graph = facebook.GraphAPI(access_token=page["access_token"])
-    graph.put_object(
-        parent_object="me",
-        connection_name="feed",
-        message=publishing.description,
-        link=publishing.link_url)
-
-
-def get_page(acc_tok):
+def get_list_user_pages(acc_tok):
+    """Return a list of dictionaries representing the Facebook pages of the user."""
     try:
         graph = facebook.GraphAPI(access_token=acc_tok)
         pages = graph.get_object('me/accounts')
         return pages['data']
     except facebook.GraphAPIError:
-        return ["error"]
+        return [{'id': '0', 'name': 'Unable to load user pages'}]
 
 
 def get_page_from_id(acc_tok, page_id):
+    """Return the dictionary corresponding to the page with id page_id"""
+    if page_id == 0:
+        return None
     try:
         graph = facebook.GraphAPI(access_token=acc_tok)
         pages = graph.get_object('me/accounts')

b849cb6e5bc2c6b1df6e5dfb22de0d42e0f0c3f8
Maxime de Streel <maxime.destreel@student.uclouvain.be>
2018-10-30 17:20:57 +0100

    Update tests from unittest to pytest
    Change from untittest to pytest and supress absolute path for more
    flexibility

--- superform/superform/tests/test_rss.py
+++ superform/superform/tests/test_rss.py
@@ -1,4 +1,3 @@
-import unittest
 import xml.etree.ElementTree as ET
 import os
 from pathlib import Path
@@ -7,45 +6,46 @@ from pathlib import Path
 from superform import *
 from superform.plugins import rss
 
-class TestRSS(unittest.TestCase):
 
-    def test_create_feed_if_none_exist(self):
-        pub = Publishing(title="coucou", description="ta mere", link_url="www.facebook.com")
-        os.system("rm /Users/maxime/serv/rss.xml")
-
-        conf = "{\"feedUrl\": \"/Users/maxime/serv/rss.xml\"}"
-
-        rss.run(pub, conf)
-
-        file = Path("/Users/maxime/serv/rss.xml")
-
-        self.assertTrue(file.exists())
-
-    def test_publish_post(self):
-        pub = Publishing(title="Voici un super beau titre", description="Avec une super description", link_url="www.facebook.com", date_from="2018-10-25")
-        conf = "{\"feedUrl\": \"/Users/maxime/serv/rss.xml\"}"
-
-        rss.run(pub, conf)
-
-        tree = ET.parse('/Users/maxime/serv/rss.xml')
-        root = tree.getroot()
-
-        count1 = 0
-
-        for child in root.iter('item'):
-            count1 += 1
-
-        rss.run(pub, conf)
-
-        tree1 = ET.parse('/Users/maxime/serv/rss.xml')
-        root1 = tree1.getroot()
-
-        count2 = 0
-        for child in root1.iter('item'):
-            count2 += 1
-
-        self.assertTrue(count1 < count2)
+local_feed_url = 'rss.xml'
 
 
-if __name__ == '__main__':
-    unittest.main(verbosity=2)
+def test_create_feed_if_none_exist():
+    pub = Publishing(title="This is a test feed", description="This is a test description feed", link_url="www.facebook.com")
+    os.system("rm ../plugins/rssfeeds/rss.xml")
+
+    print(str(Path('.').absolute()))
+    conf = "{\"local_feed_url\": \"" + str(local_feed_url) + "\", \"feed_url\": \"-\", \"feed_title\": \"-\", \"feed_description\": \"-\"}"
+
+    rss.run(pub, conf)
+
+    file = Path('../plugins/rssfeeds/rss.xml')
+
+    assert file.exists() == True
+
+
+def test_publish_post():
+    pub = Publishing(title="Voici un super beau titre", description="Avec une super description", link_url="www.facebook.com", date_from="2018-10-25")
+    conf = "{\"local_feed_url\": \"" + local_feed_url + "\", \"feed_url\": \"-\", \"feed_title\": \"-\", \"feed_description\": \"-\"}"
+
+    rss.run(pub, conf)
+
+    tree = ET.parse("../plugins/rssfeeds/rss.xml")
+    root = tree.getroot()
+
+    count1 = 0
+
+    for child in root.iter('item'):
+        count1 += 1
+
+    rss.run(pub, conf)
+
+    tree1 = ET.parse("../plugins/rssfeeds/rss.xml")
+    root1 = tree1.getroot()
+
+    count2 = 0
+    for child in root1.iter('item'):
+        count2 += 1
+
+    assert (count1 < count2) == True
+

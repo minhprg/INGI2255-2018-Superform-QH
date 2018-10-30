a295208fc189d8586ed52e56967d282ec8403f98
Maxime de Streel <maxime.destreel@student.uclouvain.be>
2018-10-30 17:45:25 +0100

    Fix: route dir project is badly defined

--- superform/superform/plugins/rss.py
+++ superform/superform/plugins/rss.py
@@ -1,3 +1,5 @@
+import os
+
 import PyRSS2Gen
 import datetime
 import json
@@ -7,57 +9,58 @@ from lxml import etree
 
 FIELDS_UNAVAILABLE = ["date_from", "date_until", "image"]
 CONFIG_FIELDS = ["feed_title", "feed_description", "feed_url", "local_feed_url"]
-rss = None
+rss_feeds = {}
 
 
 def create_initial_feed(feed_url, feed_title="Superform's RSS feed", feed_description="The RSS feed of Superform"):
 
-    global rss
-    rss = PyRSS2Gen.RSS2(
+    global rss_feeds
+    rss_feeds = PyRSS2Gen.RSS2(
         title=feed_title,
         link=feed_url,
         description=feed_description,
         items=[]
     )
 
-    return rss
+    return rss_feeds
 
 
 def run(publishing, channel_config):
-    global rss
+    global rss_feeds
 
     json_data = json.loads(channel_config)
-    rss_local_file = json_data["local_feed_url"]
-    rss_local_file_path = "./superform/plugins/rssfeeds/" + rss_local_file
-    rss_url = json_data["feed_url"]
-    rss_title = json_data["feed_title"]
-    rss_description = json_data["feed_description"]
+    rss_feeds_url = json_data["feed_url"]
+    rss_feeds_title = json_data["feed_title"]
+    rss_feeds_description = json_data["feed_description"]
+    rss_feeds_local_file = json_data["local_feed_url"]
+    rss_plugin_path = os.path.dirname(__file__)
+    rss_feeds_local_file_path = rss_plugin_path + "/rssfeeds/" + rss_feeds_local_file
 
-    file = Path(rss_local_file_path)
+    file = Path(rss_feeds_local_file_path)
 
     if not file.exists():
-        rss = create_initial_feed(feed_url=rss_url, feed_title=rss_title, feed_description=rss_description)
+        rss_feeds = create_initial_feed(feed_url=rss_feeds_url, feed_title=rss_feeds_title, feed_description=rss_feeds_description)
     else:
-        tree = etree.parse(rss_local_file_path)
+        tree = etree.parse(rss_feeds_local_file_path)
 
-        if rss is None:
-            rss = create_initial_feed(feed_url=rss_url, feed_title=rss_title, feed_description=rss_description)
+        if rss_feeds is None:
+            rss_feeds = create_initial_feed(feed_url=rss_feeds_url, feed_title=rss_feeds_title, feed_description=rss_feeds_description)
 
             title = list()
             link = list()
             description = list()
             pub_date = list()
 
-            for attrib in tree.xpath("/rss/channel/item/title"):
+            for attrib in tree.xpath("/rss_feeds/channel/item/title"):
                 title.append(attrib.text)
 
-            for attrib in tree.xpath("/rss/channel/item/link"):
+            for attrib in tree.xpath("/rss_feeds/channel/item/link"):
                 link.append(attrib.text)
 
-            for attrib in tree.xpath("/rss/channel/item/description"):
+            for attrib in tree.xpath("/rss_feeds/channel/item/description"):
                 description.append(attrib.text)
 
-            for attrib in tree.xpath("/rss/channel/item/pubDate"):
+            for attrib in tree.xpath("/rss_feeds/channel/item/pubDate"):
                 pub_date.append(datetime.datetime.strptime(attrib.text, "%a, %d %b %Y %X GMT"))
 
             for i in range(0, len(title)):
@@ -67,7 +70,7 @@ def run(publishing, channel_config):
                     description=description[i],
                     pubDate=pub_date[i]
                 )
-                rss.items.append(item)
+                rss_feeds.items.append(item)
 
     pub_date = datetime.datetime.now()
 
@@ -77,9 +80,9 @@ def run(publishing, channel_config):
                 description=publishing.description,
                 pubDate=pub_date)
 
-    rss.items.insert(0, item)
-    for post in rss.items:
+    rss_feeds.items.insert(0, item)
+    for post in rss_feeds.items:
         if post.pubDate + datetime.timedelta(365) < pub_date:
-            rss.items.remove(post)
-    with open(rss_local_file_path, "w+") as file:
-        rss.write_xml(file)
+            rss_feeds.items.remove(post)
+    with open(rss_feeds_local_file_path, "w+") as file:
+        rss_feeds.write_xml(file)

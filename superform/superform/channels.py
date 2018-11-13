c6e2ea474d974adc1958414eece5043af7869062
gauthierwala <gauthier.wala@student.uclouvain.be>
2018-11-13 17:42:49 +0100

    Changes setup

--- superform/superform/channels.py
+++ superform/superform/channels.py
@@ -1,5 +1,7 @@
 from flask import Blueprint, current_app, url_for, request, make_response, redirect, session, render_template
 from linkedin_v2 import linkedin
+import requests
+import json
 
 from superform.utils import login_required, get_instance_from_module_path, get_modules_names, get_module_full_name
 from superform.models import db, Channel
@@ -101,22 +103,37 @@ def callback_fb():
     db.session.commit()
     return redirect(url_for("channels.configure_channel", id=id_channel))
 
-@channels_page.route("/callback_In", methods=['GET', 'POST'])
+@channels_page.route("/callback_In/code", methods=['GET', 'POST'])
 @login_required(admin_required=True)
 def callback_In():
     """Page where LinkedIn returns the code to get the access token.
             Generate the access token from the code and save it to the DB."""
-    app_key = current_app.config["LINKEDIN_API_KEY"]
-    app_secret = current_app.config["LINKEDIN_API_SECRET"]
+    app_key=current_app.config["LINKEDIN_API_KEY"]
+    app_secret=current_app.config["LINKEDIN_API_SECRET"]
+    code = request.args.get('code')
+    state=request.args.get('state')
+    if "error" in request.args:
+        error = request.args.get('error')
+        print(error)
+    if(state!="9876543210"):
+        print("boom")
+        return 0
     canvas_url = url_for('channels.callback_In', _external=True)
-    authentication = linkedin.LinkedInAuthentication(app_key, app_secret, canvas_url)
-    application = linkedin.LinkedInApplication(authentication)
-    print(request.args.get('code'))
-    authentication.authorization_code = request.args.get('code')
-    token = authentication.get_access_token()
-    application = linkedin.LinkedInApplication(token=token)
-    print(token)
+    response = requests.post("https://www.linkedin.com/oauth/v2/accessToken",
+                             data={"grant_type": "authorization_code", "code": code, "redirect_uri": canvas_url,
+                                   "client_id": app_key, "client_secret": app_secret})
+    response=response.json()
+    token=response['access_token']
+    headers= {'Authorization': 'Bearer '+token,'Host':'api.linkedin.com','Connection':'Keep-Alive','x-li-format': 'json',"Content-Type":"application/json"}
+    data={"comment": "Check out developer.linkedin.com!","content": {"title": "LinkedIn Developers Resources","description": "Leverage LinkedIn's APIs to maximize engagement","submitted-url": "https://developer.linkedin.com","submitted-image-url": "https://www.catizz.com/medias/common/miaulement%20chat%20.jpg"},"visibility": {"code": "anyone"}}
+
+    data=json.dumps(data)
+
+    #response = requests.post("https://api.linkedin.com/v1/people/~/shares/?format=json",headers=headers,data=data)
+    response = requests.post("https://api.linkedin.com/v1/companies/12654611/shares/?format=json",headers=headers,data=data)
     return 0
+
+
 
 @channels_page.route("/test", methods=['GET', 'POST'])
 @login_required(admin_required=True)
@@ -124,7 +141,10 @@ def linkedInAuth():
     """Page where LinkedIn is called to get the code to have the access token."""
 
     app_key = current_app.config["LINKEDIN_API_KEY"]
-    app_secret = current_app.config["LINKEDIN_API_SECRET"]
+    result = createRequestCodeLinkedIn(app_key,"9876543210")
+    return redirect(result)
+
+def createRequestCodeLinkedIn(app_key,state):
     canvas_url = url_for('channels.callback_In', _external=True)
-    authentication = linkedin.LinkedInAuthentication(app_key, app_secret, canvas_url)
-    return redirect(authentication.authorization_url)
+    returnUrl = "https://www.linkedin.com/uas/oauth2/authorization?response_type=code&scope=rw_company_admin&client_id="+app_key+"&state="+state+"&redirect_uri="+canvas_url
+    return returnUrl

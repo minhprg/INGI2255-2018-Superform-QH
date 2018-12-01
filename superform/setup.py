091034cfe3666f7fbb5c74f0ef2d062f5320bbd1
Maxime de Streel <maxime.destreel@student.uclouvain.be>
2018-12-01 19:36:02 +0100

    Add feedparser to requires

--- superform/setup.py
+++ superform/setup.py
@@ -6,6 +6,7 @@ setup(
     include_package_data=True,
     install_requires=[
         'facebook-sdk',
+        'feedparser',
         'flask',
         'flask-sqlalchemy',
         'google-api-python-client',

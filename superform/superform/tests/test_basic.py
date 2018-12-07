8ac2c295a7590e449ed69e43778698d53278b9d7
nrybowski <nicolas.rybowski@student.uclouvain.be>
2018-12-07 19:38:09 +0100

    Fix failing tests due to typo fix into index.html

--- superform/superform/tests/test_basic.py
+++ superform/superform/tests/test_basic.py
@@ -48,7 +48,7 @@ def login(client, login):
 def test_index_not_logged_in(client):
     rv = client.get('/', follow_redirects=True)
     assert rv.status_code == 200
-    assert "Your are not logged in." in rv.data.decode()
+    assert "You are not logged in." in rv.data.decode()
 
 
 def test_other_pages_not_logged_in(client):
@@ -67,7 +67,7 @@ def test_index_logged_in(client):
     login(client, "myself")
     rv2 = client.get('/', follow_redirects=True)
     assert rv2.status_code == 200
-    assert "Your are not logged in." not in rv2.data.decode()
+    assert "You are not logged in." not in rv2.data.decode()
 
 
 def test_log_out(client):
@@ -76,7 +76,7 @@ def test_log_out(client):
     assert rv2.status_code == 200
     rv2 = client.get('/logout',follow_redirects=True)
     assert rv2.status_code == 200
-    assert "Your are not logged in." in rv2.data.decode()
+    assert "You are not logged in." in rv2.data.decode()
 
 
 def test_new_post(client):

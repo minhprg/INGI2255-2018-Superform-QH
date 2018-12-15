6be6e13de5df39584df6add1daf8f85bf6ceb8eb
nrybowski <nicolas.rybowski@student.uclouvain.be>
2018-12-15 04:22:11 +0100

    Fix ICTV hack

--- superform/superform/tests/ictv-server/server-setup.py
+++ superform/superform/tests/ictv-server/server-setup.py
@@ -1,13 +1,15 @@
 import sqlite3
 from requests import get
+from json import dumps
 
 db = sqlite3.connect('/tmp/ictv_database.sqlite')
 c = db.cursor()
+conf = {'enable_rest_api': 'true', 'api_key': 'azertyuiop'}
 
 for i in range(1,4):
     c.execute('INSERT INTO channel VALUES (' + str(i) + ', "channel-' + str(i) +' ", ?, 1, "public", "azerty' + str(i) + '", "PluginChannel")', (None,)).fetchall()
 
-    c.execute('INSERT INTO plugin_channel VALUES (' + str(i) + ', 1, "{enable_rest_api: true, api_key: azertyuiop}", 1, 60, 0, ?)', (None,)).fetchall()
+    c.execute('INSERT INTO plugin_channel VALUES (' + str(i) + ', 1, ?, 1, 60, 0, ?)', (dumps(conf),None,)).fetchall()
 
 db.commit()
 
